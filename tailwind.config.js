9b668ee62f6deddd01ec97c495c6e0e79eb74ab7
Nodari Muselishvili <85808236+sp33k1337@users.noreply.github.com>
2023-05-09 22:11:28 +0400

    header

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif']
+    },
+    extend: {}
+  },
+  plugins: [],
+}
