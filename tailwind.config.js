43fe9c60b35dbcc4109613ddbe45b8015baa8075
Nodari Muselishvili <85808236+sp33k1337@users.noreply.github.com>
2023-05-10 00:39:03 +0400

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
+    borderRadius: {
+      half: ['50px'],
+      doublexl: ['25px']
+    },
     fontFamily: {
       'poppins': ['Poppins', 'sans-serif']
     },
