7b2eef7a635142e7488a4e745db1120bcf58015b
Nodari Muselishvili <85808236+sp33k1337@users.noreply.github.com>
2023-05-11 00:53:44 +0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    width: {
-      '350px': '350px',
-      '546px': '546px',
-    },
+    // width: {
+    //   '350px': '350px',
+    //   '546px': '546px',
+    // },
     borderRadius: {
       half: ['50px'],
-      doublexl: ['25px']
+      doublexl: ['25px'],
+      xl: ['10px']
     },
     fontFamily: {
       'poppins': ['Poppins', 'sans-serif']
